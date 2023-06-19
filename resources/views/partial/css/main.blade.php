1e6ca9a80ca503932a5cd594aa21d67a6ea9484a
Rafid Redwan Khan <rafidredwankhan@gmail.com>
2023-06-19 17:21:26 +0600

    Adding HTML template to Laravel Blade

---
+++ resources/views/partial/css/main.blade.php
@@ -0,0 +1,18 @@
+<link rel="stylesheet" type="text/css" href="{{ asset('vendors/styles/core.css')}}" />
+		<link
+			rel="stylesheet"
+			type="text/css"
+			href="{{ asset('vendors/styles/icon-font.min.css')}}"
+		/>
+		<link
+			rel="stylesheet"
+			type="text/css"
+			href="{{ asset('src/plugins/datatables/css/dataTables.bootstrap4.min.css')}}"
+		/>
+		<link
+			rel="stylesheet"
+			type="text/css"
+			href="{{ asset('src/plugins/datatables/css/responsive.bootstrap4.min.css')}}"
+		/>
+		<link rel="stylesheet" type="text/css" href="{{ asset('vendors/styles/style.css')}}" />
+        @stack('extra-css')
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use App\Http\Controllers\PostController;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts/master');
 });
 
  
